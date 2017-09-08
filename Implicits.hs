226bd0d28b8c04bbedbb2a11a6255bc2989f4956
Alex Jeffery <apjeffery136@googlemail.com>
2017-09-08 17:45:38 +0100

    Cont impl

--- Implicits.hs
+++ Implicits.hs
@@ -43,26 +43,74 @@ data Type
 -- TYPING
 --------------------------------------------------------------------------------
 
-type Env = M.Map Name Type
+tySub :: Name -> Type -> Type -> Type
+tySub = undefined
 
-inferType :: Env -> Env -> Term -> Maybe Type
-inferType explEnv implEnv term = case term of
-    EVar x   -> M.lookup x explEnv
+data Env = Env
+    { explicits :: M.Map Name Type
+    , implicits :: M.Map Name Type
+    , typeVars  :: S.Set Name
+    }
+
+insertE, insertI :: Name -> Type -> Env -> Env
+insertE x t (Env expl impl vars) = Env (M.insert x t expl) impl vars
+insertI x t (Env expl impl vars) = Env expl (M.insert x t impl) vars
+insertV :: Name -> Env -> Env
+insertV x (Env expl impl vars) = Env expl impl (S.insert x vars)
+
+inferType :: Env -> Term -> Maybe Type
+inferType env term = case term of
+    -- (Var)
+    EVar x -> M.lookup x (explicits env)
+
+    -- (Query)
+    IQuery -> undefined
+
+    -- (-> E)
     App t t' -> do
-        EFun ty1 ty2 <- inferType explEnv implEnv t
-        if checkType explEnv implEnv t' ty1 then Just ty2 else Nothing
+        EFun ty1 ty2 <- inferType env t
+        if checkType env t' ty1 then Just ty2 else Nothing
+
+    -- (Let-Ex)
+    ELet x ty t t' ->
+        if checkType env t ty then inferType (insertE x ty env) t' else Nothing
+
+    -- (Let-Im)
+    ILet ty t t' ->
+        if checkType env t ty then
+            inferType (insertE undefined ty env) t'
+        else Nothing
+
     TrueLit  -> Just TBool
     FalseLit -> Just TBool
-    _        -> undefined
 
-checkType :: Env -> Env -> Term -> Type -> Bool
-checkType explEnv implEnv term ty = case term of
-    Lam x t    -> case ty of
-        EFun ty1 ty2 -> checkType (M.insert x ty1 explEnv) implEnv t ty2
+    _ -> case inferType env term of
+        -- (?-> E)
+        Just (IFun ty1 ty2) ->
+            if checkType env IQuery ty1 then Just ty2 else Nothing
+
+        -- (V E)
+        Just (Forall x ty) -> Just $ tySub x undefined ty
+
+        _ -> undefined
+
+checkType :: Env -> Term -> Type -> Bool
+checkType env term ty = case term of
+    -- (-> I)
+    Lam x t -> case ty of
+        EFun ty1 ty2 -> checkType (insertE x ty1 env) t ty2
+
     If tb t t' ->
-        checkType explEnv implEnv tb TBool
-        && checkType explEnv implEnv t  ty
-        && checkType explEnv implEnv t' ty
-    _          -> case ty of
-        IFun ty1 ty2 -> undefined -- rule (?-> I)
-        _            -> inferType explEnv implEnv term == Just ty
+        checkType env tb TBool
+        && checkType env t  ty
+        && checkType env t' ty
+
+    _ -> case ty of
+        -- (?-> I)
+        IFun ty1 ty2 -> undefined
+
+        -- (V I)
+        Forall x ty  -> checkType (insertV x env) term ty
+
+        -- (Stitch)
+        _ -> inferType env term == Just ty
