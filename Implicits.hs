323c6831733c3eae1e081a843d735890c719040a
Alex Jeffery <apjeffery136@googlemail.com>
2017-09-11 17:55:38 +0100

    Cont impl

--- Implicits.hs
+++ Implicits.hs
@@ -1,13 +1,11 @@
 module Main where
 
 -- Implementation of the calculus in the paper "Foundations of implicit
--- function types" - Odersky et al.
+-- function types" - Odersky et al. We omit the Forall type here for simplicity.
 
 import qualified Data.Map as M
 import qualified Data.Set as S
 import Control.Monad.State
-import Control.Monad.Writer
-import Control.Monad.Except
 
 main :: IO ()
 main = return ()
@@ -20,51 +18,63 @@ type Name = Integer
 
 data Term
     = EVar Name
-    | IVar Name
     | Lam Name Term
     | App Term Term
-    | IQuery
     | ELet Name Type Term Term
-    | ILet Type Term Term
     | If Term Term Term
     | TrueLit
     | FalseLit
+    | IVar Name
+    | ILet Type Term Term
+    | IQuery
     deriving (Show, Eq, Ord)
 
 data Type
-    = TVar Name
-    | EFun Type Type
+    = EFun Type Type
     | IFun Type Type
-    | Forall Name Type
     | TBool
+    deriving (Show, Eq, Ord)
+
+data PTerm
+    = PVar Name
+    | PLam Name PTerm
+    | PApp PTerm PTerm
+    | PLet Name PTerm PTerm
+    | PIf PTerm PTerm PTerm
+    | PTrueLit
+    | PFalseLit
     deriving (Show, Eq, Ord)
 
 --------------------------------------------------------------------------------
 -- TYPING
 --------------------------------------------------------------------------------
 
-tySub :: Name -> Type -> Type -> Type
-tySub = undefined
-
 data Env = Env
     { explicits :: M.Map Name Type
     , implicits :: M.Map Name Type
-    , typeVars  :: S.Set Name
-    }
+    } deriving (Show, Eq, Ord)
 
-insertE, insertI :: Name -> Type -> Env -> Env
-insertE x t (Env expl impl vars) = Env (M.insert x t expl) impl vars
-insertI x t (Env expl impl vars) = Env expl (M.insert x t impl) vars
-insertV :: Name -> Env -> Env
-insertV x (Env expl impl vars) = Env expl impl (S.insert x vars)
+emptyEnv :: Env
+emptyEnv = Env M.empty M.empty
 
-inferType :: Env -> Term -> Maybe Type
+envInsertExplicit :: Name -> Type -> Env -> Env
+envInsertExplicit x t (Env expl impl) = Env (M.insert x t expl) impl
+
+envInsertImplicit :: Name -> Type -> Env -> Env
+envInsertImplicit y t (Env expl impl) = Env expl (M.insert y t impl)
+
+fresh :: StateT Name Maybe Name
+fresh = state $ \s -> (s, s + 1)
+
+inferType :: Env -> Term -> StateT Name Maybe (Type, PTerm)
 inferType env term = case term of
     -- (Var)
-    EVar x -> M.lookup x (explicits env)
+    EVar x -> do
+        ty <- lift $ M.lookup x (explicits env)
+        return (ty, PVar x)
 
     -- (Query)
-    IQuery -> undefined
+    IQuery -> S.lookupMax (implicits env)
 
     -- (-> E)
     App t t' -> do
@@ -73,12 +83,14 @@ inferType env term = case term of
 
     -- (Let-Ex)
     ELet x ty t t' ->
-        if checkType env t ty then inferType (insertE x ty env) t' else Nothing
+        if checkType env t ty then
+            inferType (envInsertExplicit x ty env) t'
+        else Nothing
 
     -- (Let-Im)
     ILet ty t t' ->
         if checkType env t ty then
-            inferType (insertE undefined ty env) t'
+            inferType (envInsertImplicit undefined ty env) t'
         else Nothing
 
     TrueLit  -> Just TBool
@@ -89,28 +101,31 @@ inferType env term = case term of
         Just (IFun ty1 ty2) ->
             if checkType env IQuery ty1 then Just ty2 else Nothing
 
-        -- (V E)
-        Just (Forall x ty) -> Just $ tySub x undefined ty
+        _ -> lift Nothing
 
-        _ -> undefined
-
-checkType :: Env -> Term -> Type -> Bool
+checkType :: Env -> Term -> Type -> StateT Name Maybe PTerm
 checkType env term ty = case term of
     -- (-> I)
     Lam x t -> case ty of
-        EFun ty1 ty2 -> checkType (insertE x ty1 env) t ty2
+        EFun ty1 ty2 -> do
+            tTrans <- checkType (envInsertExplicit x ty1 env) t ty2
+            return $ PLam x tTrans
+        _ -> error "Lam checkType not given EFun"
 
-    If tb t t' ->
-        checkType env tb TBool
-        && checkType env t  ty
-        && checkType env t' ty
+    If gd tb fb -> do
+        gdTrans <- checkType env gd TBool
+        tbTrans <- checkType env tb ty
+        fbTrans <- checkType env fb ty
+        return $ PIf gdTrans tbTrans fbTrans
 
     _ -> case ty of
         -- (?-> I)
-        IFun ty1 ty2 -> undefined
-
-        -- (V I)
-        Forall x ty  -> checkType (insertV x env) term ty
+        IFun ty1 ty2 -> do
+            y <- fresh
+            trans <- checkType (envInsertImplicit y ty1 env) term ty2
+            return $ PLam y trans
 
         -- (Stitch)
-        _ -> inferType env term == Just ty
+        _ -> do
+            (inferredTy, trans) <- inferType env term
+            if inferredTy == ty then return trans else lift Nothing
